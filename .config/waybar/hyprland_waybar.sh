9b415fb3e76908ae01174fb81906ad4b865c7903
tofu-salad <67925799+tofu-salad@users.noreply.github.com>
2025-02-19 01:43:59 -0300

    better waybar config that isnt a mess

--- .config/waybar/hyprland_waybar.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-#
-waybar -c "$HOME/.config/waybar/conf.d/hyprland/hyprland.conf" -s "$HOME/.config/waybar/conf.d/style.css"
--- .config/waybar/sway_waybar.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-#
-waybar -c "$HOME/.config/waybar/conf.d/sway/sway.conf" -s "$HOME/.config/waybar/conf.d/style.css"
