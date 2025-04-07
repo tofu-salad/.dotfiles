6e5e6e7aa508fe21488ad6d9f58dfe369a3c147a
tofu-salad <67925799+tofu-salad@users.noreply.github.com>
2025-04-07 08:32:47 -0300

    small details

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -1,7 +1,7 @@
-vim.opt.expandtab = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.softtabstop = 4
+-- vim.opt.expandtab = true
+-- vim.opt.tabstop = 4
+-- vim.opt.shiftwidth = 4
+-- vim.opt.softtabstop = 4
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
