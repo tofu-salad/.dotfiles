f260f557c28767c687970cbcc3ded570ca783713
tofu-salad <67925799+tofu-salad@users.noreply.github.com>
2025-08-09 12:20:48 -0300

    new nvim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,44 +1,4 @@
 return {
-	-- git plugins
-	{
-		"lewis6991/gitsigns.nvim",
-		opts = {
-			signs = {
-				add = { text = "+" },
-				change = { text = "~" },
-				delete = { text = "_" },
-				topdelete = { text = "‾" },
-				changedelete = { text = "~" },
-			},
-		},
-	},
-	{
-		"tpope/vim-fugitive",
-		config = function()
-			vim.keymap.set("n", "<leader>ga", "<cmd>Git<CR>")
-			vim.keymap.set("n", "<leader>gc", "<cmd>Git commit<CR>")
-			vim.keymap.set("n", "<leader>gm", "<cmd>Git mergetool<CR>")
-			vim.keymap.set("n", "<leader>gp", "<cmd>Git push<CR>")
-		end,
-	},
-	"tpope/vim-sleuth",
-	{
-		"williamboman/mason.nvim",
-		lazy = true,
-		cmd = "Mason",
-		opts = {},
-	},
-	{
-		"j-hui/fidget.nvim",
-		opts = {
-			notification = {
-				window = {
-					winblend = 100,
-					zindex = 55,
-				},
-			},
-		},
-	},
 	-- format and lint
 	{
 		"stevearc/conform.nvim",
@@ -52,63 +12,57 @@ return {
 			require("plugins.conf.nvim-lint")
 		end,
 	},
-	-- autocompletion
 	{
-		"saghen/blink.cmp",
-		dependencies = "rafamadriz/friendly-snippets",
-		version = "*",
-		config = function()
-			require("plugins.conf.completion")
-		end,
-	},
-	-- highlight, edit, and navigate code
-	{
-		"nvim-treesitter/nvim-treesitter",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter-textobjects",
-			"JoosepAlviste/nvim-ts-context-commentstring",
-		},
-		build = ":TSUpdate",
-		config = function()
-			require("plugins.conf.treesitter")
-		end,
-	},
-	{
-		"nvim-telescope/telescope.nvim",
-		event = "VeryLazy",
-		branch = "0.1.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			{
-				"nvim-telescope/telescope-fzf-native.nvim",
-				build = "make",
-				cond = function()
-					return vim.fn.executable("make") == 1
-				end,
+		"j-hui/fidget.nvim",
+		opts = {
+			notification = {
+				window = {
+					winblend = 100,
+					zindex = 55,
+				},
 			},
 		},
-		config = function()
-			require("plugins.conf.telescope")
-		end,
 	},
 	{
-		"ThePrimeagen/harpoon",
-		keys = { "<leader>h" },
+		"datsfilipe/vesper.nvim",
 		config = function()
-			require("plugins.conf.harpoon")
+			require("vesper").setup({
+				transparent = false, -- Boolean: Sets the background to transparent
+				italics = {
+					comments = true, -- Boolean: Italicizes comments
+					keywords = false, -- Boolean: Italicizes keywords
+					functions = false, -- Boolean: Italicizes functions
+					strings = false, -- Boolean: Italicizes strings
+					variables = false, -- Boolean: Italicizes variables
+				},
+				overrides = {}, -- A dictionary of group names, can be a function returning a dictionary or a table.
+				palette_overrides = {
+					bg = "#161616",
+				},
+			})
+			vim.cmd.colorscheme("vesper")
 		end,
 	},
+	-- {
+	-- 	"nyoom-engineering/oxocarbon.nvim",
+	-- 	config = function()
+	-- 		-- vim.opt.background = "dark"
+	-- 		-- vim.cmd.colorscheme("oxocarbon")
+	-- 	end,
+	-- },
 	{
-		"NvChad/nvim-colorizer.lua",
+		"echasnovski/mini.pick",
+		dependencies = { "echasnovski/mini.extra" },
 		config = function()
-			require("plugins.conf.colorizer")
+			require("mini.pick").setup()
+			require("mini.extra").setup()
+			vim.keymap.set("n", "<leader>?", ":Pick help<CR>", { silent = true })
+			vim.keymap.set("n", "<leader><leader>", ":Pick buffers<CR>", { silent = true })
+			vim.keymap.set("n", "<leader>ft", ":Pick git_files<CR>", { silent = true })
+			vim.keymap.set("n", "<leader>ff", ":Pick files<CR>", { silent = true })
+			vim.keymap.set("n", "<leader>fw", ":Pick grep<CR>", { silent = true })
+			vim.keymap.set("n", "<leader>fg", ":Pick grep_live<CR>", { silent = true })
+			vim.keymap.set("n", "<leader>fd", ":Pick diagnostic<CR>", { silent = true })
 		end,
 	},
-	{
-		"echasnovski/mini.statusline",
-		config = function()
-			require("plugins.conf.statusline")
-		end,
-	},
-	require("plugins.conf.themes"),
 }
