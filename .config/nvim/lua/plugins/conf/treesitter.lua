c1d5bf48a682c428da6b664db586027927b5d418
tofu salad <tofu salad>
2025-10-07 18:09:52 -0300

    small

--- .config/nvim/lua/plugins/conf/treesitter.lua
+++ .config/nvim/lua/plugins/conf/treesitter.lua
@@ -1,75 +1,23 @@
 -- typescript comments
+require("ts_context_commentstring").setup({
+	enable_autocmd = false,
+})
 local get_option = vim.filetype.get_option
 
+---@diagnostic disable-next-line: duplicate-set-field
 vim.filetype.get_option = function(filetype, option)
 	return option == "commentstring" and require("ts_context_commentstring.internal").calculate_commentstring()
 		or get_option(filetype, option)
 end
 
-require("ts_context_commentstring").setup({
-	enable_autocmd = false,
-})
-
 require("nvim-treesitter.configs").setup({
-	ensure_installed = { "go", "lua", "python", "rust", "tsx", "javascript", "typescript", "vimdoc", "vim" },
-
+	ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+	sync_install = false,
 	auto_install = true,
-
+	ignore_install = {},
 	highlight = {
 		enable = true,
+		disable = {},
 	},
 	indent = { enable = true },
-	incremental_selection = {
-		enable = true,
-		keymaps = {
-			init_selection = "<c-space>",
-			node_incremental = "<c-space>",
-			scope_incremental = "<c-s>",
-			node_decremental = "<M-space>",
-		},
-	},
-	textobjects = {
-		select = {
-			enable = true,
-			lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-			keymaps = {
-				-- You can use the capture groups defined in textobjects.scm
-				["aa"] = "@parameter.outer",
-				["ia"] = "@parameter.inner",
-				["af"] = "@function.outer",
-				["if"] = "@function.inner",
-				["ac"] = "@class.outer",
-				["ic"] = "@class.inner",
-			},
-		},
-		move = {
-			enable = true,
-			set_jumps = true, -- whether to set jumps in the jumplist
-			goto_next_start = {
-				["]m"] = "@function.outer",
-				["]]"] = "@class.outer",
-			},
-			goto_next_end = {
-				["]M"] = "@function.outer",
-				["]["] = "@class.outer",
-			},
-			goto_previous_start = {
-				["[m"] = "@function.outer",
-				["[["] = "@class.outer",
-			},
-			goto_previous_end = {
-				["[M"] = "@function.outer",
-				["[]"] = "@class.outer",
-			},
-		},
-		swap = {
-			enable = true,
-			swap_next = {
-				["<leader>a"] = "@parameter.inner",
-			},
-			swap_previous = {
-				["<leader>A"] = "@parameter.inner",
-			},
-		},
-	},
 })
