0b6820f28fd8a94824e2c15098297d90ab9be154
tofu-salad <67925799+tofu-salad@users.noreply.github.com>
2026-03-10 15:44:44 -0300

    testing it

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,26 +1,23 @@
 -- options
-vim.g.have_nerd_font = false
-vim.o.mouse = "a"
-
-vim.o.hlsearch = false
-vim.o.inccommand = "split"
-vim.o.incsearch = true
-
-vim.o.scrolloff = 10
-vim.o.sidescrolloff = 10
-vim.o.wrap = false
-
-vim.o.colorcolumn = "80"
 vim.o.number = true
 vim.o.relativenumber = true
+vim.o.scrolloff = 8
+vim.o.cursorline = true
+vim.o.sidescrolloff = 8
+vim.o.wrap = false
+vim.o.laststatus = 3
+vim.o.mouse = "a"
+
+vim.o.colorcolumn = "80"
 vim.o.signcolumn = "yes"
 
 vim.o.breakindent = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
 
-vim.o.timeoutlen = 300
-vim.o.updatetime = 250
+vim.o.hlsearch = false
+vim.o.inccommand = "split"
+vim.o.incsearch = true
 
 vim.o.swapfile = false
 vim.o.undofile = true
@@ -28,21 +25,77 @@ vim.o.undofile = true
 vim.o.splitbelow = true
 vim.o.splitright = true
 
-vim.o.laststatus = 3
-vim.opt.completeopt = "menu,noselect,fuzzy,nosort"
+vim.o.completeopt = "menu,noselect,fuzzy,nosort"
+vim.o.timeoutlen = 300
+vim.o.updatetime = 250
+
+-- netrw
+vim.g.netrw_banner = 0
+vim.g.netrw_localcopydircmd = "cp -r"
+vim.g.netrw_use_errorwindow = 0
+vim.g.netrw_keepdir = 1
+
+-- commands
+vim.api.nvim_create_user_command('LspInfo', function()
+	vim.cmd('checkhealth vim.lsp')
+end, {})
+
+vim.api.nvim_create_user_command('FindCwd', function(opts)
+	local input = opts.args
+	if input == '' then return end
+	local cwd = vim.fn.getcwd() .. '/'
+	vim.cmd('edit ' .. vim.fn.fnameescape(cwd .. input))
+end, {
+	nargs = 1,
+	complete = function(arglead)
+		local cwd = vim.fn.getcwd() .. '/'
+		local matches = vim.fn.systemlist('fd --type f --max-results 20 --glob ' ..
+			vim.fn.shellescape('*' .. arglead .. '*') .. ' ' .. vim.fn.shellescape(cwd))
+		return vim.tbl_map(function(f)
+			return f:gsub('^' .. vim.pesc(cwd), '')
+		end, matches)
+	end,
+})
+
+vim.api.nvim_create_user_command('FindGit', function(opts)
+	local input = opts.args
+	if input == '' then return end
+
+	local files = vim.fn.systemlist('git ls-files')
+	local matches = vim.tbl_filter(function(f)
+		return f:find(input, 1, true) ~= nil
+	end, files)
+
+	if #matches == 0 then
+		vim.notify('No files found: ' .. input, vim.log.levels.WARN)
+	else
+		vim.cmd('edit ' .. vim.fn.fnameescape(matches[1]))
+	end
+end, {
+	nargs = 1,
+	complete = function(arglead)
+		local files = vim.fn.systemlist('git ls-files')
+		return vim.tbl_filter(function(f)
+			return f:find(arglead, 1, true) ~= nil
+		end, files)
+	end,
+})
+
 -- keymaps
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 vim.keymap.set("v", "<leader>y", '"+y', { silent = true, desc = "[y]ank to system clipbloard" })
 vim.keymap.set("v", "<", "<gv", { silent = true, desc = "indent left without leaving visual mode [<]" })
 vim.keymap.set("v", ">", ">gv", { silent = true, desc = "indent right without leaving visual mode [>]" })
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "move text up in visual mode [J]" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "move text down in visual mode [K]" })
-vim.keymap.set("n", "<leader>d", '[["_d]]')
-vim.keymap.set("x", "<leader>p", '[["_dP')
 vim.keymap.set("n", "<leader>e", "<cmd>Explore<CR>", { desc = "open netrw" })
-vim.keymap.set("n", "]q", "<Cmd>cn<CR>", { desc = "go to next quicklist item ']q'" })
-vim.keymap.set("n", "[q", "<Cmd>cp<CR>", { desc = "go to previous quicklist item '[q'" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "half page down (centered)" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "half page up (centered)" })
+
+vim.keymap.set("n", "]q", ":cnext<CR>", { desc = "go to next quicklist item ']q'" })
+vim.keymap.set("n", "[q", ":cprevious<CR>", { desc = "go to previous quicklist item '[q'" })
 vim.keymap.set("n", "[d", function()
 	vim.diagnostic.jump({ count = -1, float = true })
 end, { desc = "go to previos diagnostic message '[d'" })
@@ -56,11 +109,40 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "move focus to the upper window" })
 
--- netrw
-vim.g.netrw_banner = 0
-vim.g.netrw_localcopydircmd = "cp -r"
-vim.g.netrw_use_errorwindow = 0
-vim.g.netrw_keepdir = 1
+
+vim.keymap.set('n', '<leader>ff', ':FindCwd ', { desc = "[f]ind [f]iles" })
+vim.keymap.set('n', '<leader>ft', ':FindGit ', { desc = '[f]ind gi[t] files' })
+vim.keymap.set('n', '<leader>fg', function()
+	vim.ui.input({ prompt = 'Grep > ' }, function(input)
+		if not input or input == '' then return end
+		vim.cmd('silent grep ' .. input)
+		vim.cmd('copen')
+	end)
+end, { desc = '[f]iles [g]rep' })
+
+-- autocmds
+
+-- [[ highlight on yank ]]
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.hl.on_yank({ timeout = 50 })
+	end,
+	group = highlight_group,
+	pattern = "*",
+})
+
+-- [[ vertical help ]]
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "help",
+	command = "wincmd L"
+})
+
+-- [[ auto resize splits ]]
+vim.api.nvim_create_autocmd("VimResized", {
+	command = "wincmd ="
+})
 
 -- lsp
 vim.diagnostic.config({
@@ -102,22 +184,18 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		if client then
 			client.server_capabilities.semanticTokensProvider = nil
 		end
+
 		-- keybinds
 		map("<leader>lr", vim.lsp.buf.rename, "[l]sp [r]ename")
+		map("<leader>lf", vim.lsp.buf.format, "[l]sp [f]ormat")
 		map("<leader>la", vim.lsp.buf.code_action, "[l]sp code [a]ction")
 
 		map("gd", vim.lsp.buf.definition, "[g]oto [d]efinition")
-		map("<leader>gr", function()
-			require("mini.extra").pickers.lsp({ scope = "references" })
-		end, "[g]oto [r]eferences")
+		map("<leader>gr", vim.lsp.buf.references, "[g]oto [r]eferences")
 		map("gI", vim.lsp.buf.implementation, "[g]oto [I]mplementation")
 		map("<leader>D", vim.lsp.buf.type_definition, "type [D]efinition")
-		map("<leader>ds", function()
-			require("mini.extra").pickers.lsp({ scope = "document_symbol" })
-		end, "[d]ocument [s]ymbols")
-		map("<leader>ws", function()
-			require("mini.extra").pickers.lsp({ scope = "workspace_symbol" })
-		end, "[w]orkspace [s]ymbols")
+		map("<leader>ds", vim.lsp.buf.document_symbol, "[d]ocument [s]ymbols")
+		map("<leader>ws", vim.lsp.buf.workspace_symbol, "[w]orkspace [s]ymbols")
 
 		-- see `:help K` for why this keymap
 		map("K", function()
@@ -135,19 +213,6 @@ vim.api.nvim_create_autocmd("LspAttach", {
 	end,
 })
 
--- autocmds
--- [[ highlight on yank ]]
-local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
-
-vim.api.nvim_create_autocmd("TextYankPost", {
-	callback = function()
-		vim.hl.on_yank({ timeout = 50 })
-	end,
-	group = highlight_group,
-	pattern = "*",
-})
-
--- bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 ---@diagnostic disable-next-line: undefined-field
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -156,7 +221,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	if vim.v.shell_error ~= 0 then
 		vim.api.nvim_echo({
 			{ "failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{ out, "WarningMsg" },
+			{ out,                            "WarningMsg" },
 			{ "\npress any key to exit..." },
 		}, true, {})
 		vim.fn.getchar()
@@ -164,5 +229,60 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	end
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup({ spec = { { import = "plugins" } } })
-require("filetypes")
+
+require("lazy").setup({
+	{
+		"jesseleite/noirbuddy.nvim",
+		dependencies = {
+			{ "tjdevries/colorbuddy.nvim" }
+		},
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
+	{
+		"j-hui/fidget.nvim",
+		opts = {},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		lazy = false,
+		build = ":TSUpdate",
+		config = function()
+			local ts = require("nvim-treesitter")
+
+			local ignore_parsers = {
+				jsonc = true,
+			}
+
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = "*",
+				callback = function(ctx)
+					local ft = ctx.match
+					if not ft or ft == "" then
+						return
+					end
+
+					local lang = vim.treesitter.language.get_lang(ft)
+					if not lang or ignore_parsers[lang] then
+						return
+					end
+
+					local available_parsers = ts.get_available()
+					if not vim.tbl_contains(available_parsers, lang) then
+						return
+					end
+
+					local installed = ts.get_installed("parsers")
+					if not vim.tbl_contains(installed, lang) then
+						ts.install({ lang })
+						return
+					end
+
+					pcall(vim.treesitter.start, ctx.buf)
+					vim.bo[ctx.buf].indentexpr = "v:lua.require('nvim-treesitter').indentexpr()"
+				end,
+			})
+		end,
+	},
+})
