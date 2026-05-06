7e473de090581f0eac46499caf2566dbd29b2380
tofu-salad <67925799+tofu-salad@users.noreply.github.com>
2026-05-06 16:58:09 -0300

    simpler config

--- .config/waybar/scripts/powermenu-fuzzel.sh
+++ .config/waybar/scripts/powermenu-fuzzel.sh
@@ -5,18 +5,18 @@ options="⏻ Shutdown
 ⍇ Logout"
 
 choice=$(printf "%s\n" "$options" | \
-fuzzel --dmenu \
-  --font "Adwaita Mono:size=16" \
-  --hide-prompt \
-  --anchor top-right \
-  --minimal-lines \
-  --width 15 \
-  --keyboard-focus=on-demand \
-  --use-bold \
-  --border-radius 0)
+	fuzzel --dmenu \
+	--font "Adwaita Mono:size=16" \
+	--hide-prompt \
+	--anchor top-right \
+	--minimal-lines \
+	--width 15 \
+	--keyboard-focus=on-demand \
+	--use-bold \
+	--border-radius 0)
 
 case "$choice" in
-  "⏻ Shutdown") systemctl poweroff ;;
-  "↻ Reboot") systemctl reboot ;;
-  "⍇ Logout") loginctl terminate-user "$USER" ;;
+	"⏻ Shutdown") systemctl poweroff ;;
+	"↻ Reboot") systemctl reboot ;;
+	"⍇ Logout") loginctl terminate-user "$USER" ;;
 esac
