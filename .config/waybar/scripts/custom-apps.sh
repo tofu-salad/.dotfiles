c5fe4561bac6b182fd22f36730705cfda30156e7
tofu-salad <67925799+tofu-salad@users.noreply.github.com>
2026-05-15 06:39:31 -0300

    misc

--- .config/waybar/scripts/custom-apps.sh
+++ .config/waybar/scripts/custom-apps.sh
@@ -3,7 +3,7 @@
 args=(-a top-left --border-radius=0)
 
 if command -v uwsm >/dev/null 2>&1; then
-	args+=(--launch-prefix='uwsm app --')
+	args+=(--launch-prefix='uwsm-app --')
 fi
 
 fuzzel "${args[@]}"
